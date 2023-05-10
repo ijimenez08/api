b19d3922de12b3b3fb02d84a88409731e0c61ec4
ijimenez08 <isaac.jmimenez@utp.ac.pa>
2023-05-10 16:58:20 -0500

    constructores 	modified:   src/main/java/med/voll/api/Direccion/DatosDireccion.java 	modified:   src/main/java/med/voll/api/controller/MedicoController.java 	modified:   src/main/java/med/voll/api/Direccion/Direccion.java 	modified:   src/main/java/med/voll/api/medico/medico.java 	src/main/java/med/voll/api/medico/MedicoRespository.java

--- src/main/java/med/voll/api/Direccion/DatosDireccion.java
+++ src/main/java/med/voll/api/Direccion/DatosDireccion.java
@@ -2,5 +2,17 @@ package med.voll.api.Direccion;
 
 public record DatosDireccion(String direccion, String corregimiento, String distrito, String provincia) 
 {
+
+    public Integer numero() {
+        return null;
+    }
+
+    public String ciudad() {
+        return null;
+    }
+
+    public String calle() {
+        return null;
+    }
     
 }
--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,9 +1,11 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.*;
 
 
 import med.voll.api.medico.DatosRegistroMedico;
@@ -12,11 +14,17 @@ import med.voll.api.medico.DatosRegistroMedico;
 @RequestMapping("/medicos")
 public class MedicoController 
 {
+
+    @Autowired
+    private MedicoRespository medicoRespository;
+
     @PostMapping
     public void registrarMedicos(@RequestBody DatosRegistroMedico datosRegistroMedico)
     {
-        System.out.println("El Request llega correctamente");
+       /*  System.out.println("El Request llega correctamente");
         System.out.println(datosRegistroMedico);
+        */
+        medicoRespository.save(new medico (datosRegistroMedico));
 
     }
 }
