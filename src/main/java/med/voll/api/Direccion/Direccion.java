296559a9966007e2ddb4600eea91e626a4cd1fa6
ijimenez08 <isaac.jmimenez@utp.ac.pa>
2023-05-10 16:58:44 -0500

    uopdate 	modified:   src/main/java/med/voll/api/Direccion/Direccion.java 	new file:   src/main/java/med/voll/api/medico/MedicoRespository.java 	modified:   src/main/java/med/voll/api/medico/medico.java

--- src/main/java/med/voll/api/Direccion/Direccion.java
+++ src/main/java/med/voll/api/Direccion/Direccion.java
@@ -1,14 +1,29 @@
 package med.voll.api.Direccion;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Direccion 
 {
     
+   
     private String calle;
     private Integer numero;
     private String ciudad;
     private String distrito;
+
+    public Direccion(DatosDireccion direccion) 
+    {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.ciudad = direccion.ciudad();
+        this.distrito = direccion.distrito();
+    }
 
 }
--- src/main/java/med/voll/api/medico/medico.java
+++ src/main/java/med/voll/api/medico/medico.java
@@ -1,22 +1,39 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.Direccion.Direccion;
 @Table(name = "medico")
 @Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
 public class medico
 {
+   
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nombre;
     private String email;
-    private String documento;
+    private String cedula;
     @Enumerated(EnumType.STRING)
-    private String especialidad;
+    private Especialidad especialidad;
     @Embedded
     private Direccion direccion;
 
-    
+    public medico(DatosRegistroMedico datosRegistroMedico)
+    {
+        this.nombre=datosRegistroMedico.nombre();
+        this.email=datosRegistroMedico.email();
+        this.cedula=datosRegistroMedico.cedula();
+        this.especialidad=datosRegistroMedico.especialidad();
+
+        this.direccion=new Direccion(datosRegistroMedico.direccion());
+    }
 }
